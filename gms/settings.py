06555c929963ce9f2945a5c14814a7a553196d5c
Kotarooopo <134988533+Kotarooopo@users.noreply.github.com>
2024-11-15 21:01:49 +0800

    asda

--- gms/settings.py
+++ gms/settings.py
@@ -159,7 +159,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 AUTH_USER_MODEL = 'core.User'
 
-ALLOWED_HOSTS = ['192.168.1.78', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.78', 'localhost', '127.0.0.1' , 'keybeen172.pythonanywhere.com']
 # ALLOWED_HOSTS = ['.vercel.app', 'core.vercel.app', 'railway.app']
 
 
